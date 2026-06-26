ea59a6efb48ea54d97cabd6799313ddcab92360d
wadhah hermessi <wadhah.hermessi@enicar.ucar.tn>
2026-06-26 21:21:54 +0100

    feat: Added create expense method

--- src/services/expense.service.ts
+++ src/services/expense.service.ts
@@ -11,3 +11,35 @@ calculate total spending
 calculate spending by category
 calculate unpaid expenses
 */  
+import { Expense } from "../expenses/entities/expense.entity.js";
+import { ExpenseRepository} from "../repositories/expense.repository.js";
+import {type  ExpenseCategory } from "../types/expense-category.type.js";
+import {type CreateExpenseDto } from "../dto/create-expense.dto.js";
+import { type UpdateExpenseDto } from "../dto/update-expense.dto.js";
+export class ExpenseService{
+  private expenserepository: ExpenseRepository;
+  private nextId: number=1;
+
+  constructor(expenserepository: ExpenseRepository){
+    this.expenserepository=expenserepository;
+  }
+
+  private generateId(): number{
+    const id=this.nextId;
+    this.nextId++;
+    return id;
+  }
+  createExpense(expensedto: CreateExpenseDto){
+    const id=this.generateId();
+    const date= new Date();
+    const ispaid: boolean=expensedto.isPaid ?? false
+    const expense=new Expense(id, expensedto.title, expensedto.amount, expensedto.category, date,ispaid )
+    return this.expenserepository.saveExpense(expense);
+    
+  }
+
+
+
+
+
+}
